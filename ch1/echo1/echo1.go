a6237ce8b30013d633cc475dbe3ae6edabcde650
Tom <devtomx@gmail.com>
2023-02-11 14:35:36 -0300

    echo 1 exercise

---
+++ ch1/echo1/echo1.go
@@ -0,0 +1,16 @@
+package main
+
+// Echo1 exibe seus argumentos de linha de comando
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
