8a8c437a9a383dfe255358b7e288febe633997d9
Tom <devtomx@gmail.com>
2023-02-15 07:40:38 -0300

    add exercise fetchall - goroutines

---
+++ ch1/fetchall.go
@@ -0,0 +1,40 @@
+// FetchAll busca URLs em paralelo e informa os tempos gastos e os tamanhos.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // inicia uma goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // recebe do channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // envia para o channel ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // evita o vazamento de recursos
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
