7afa8bc8070bde92946e046b68c1b7a68b0ba57c
Tom <devtomx@gmail.com>
2023-02-21 18:58:32 -0300

    exercise ch4 treesort

---
+++ ch4/treesort/main.go
@@ -0,0 +1,40 @@
+package main
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort ordena valores in-place
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues concatena os elementos de t a values na ordem
+// e devolve a fatia resultante
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalente a devolver &tree{value: value}
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
