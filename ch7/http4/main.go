65aab19437a6be987daa9d88fc642ca763168e9b
Tom <devtomx@gmail.com>
2023-03-08 08:39:38 -0300

    ch7 exercise http4

---
+++ ch7/http4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/list", db.list)
+	mux.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
+
+type database map[string]int
+
+func (db database) list(w http.ResponseWriter, r *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: $%d\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "$%d\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
