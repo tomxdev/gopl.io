84e733835ce7adb2d5ea75689480e1295f0f81a7
Tom <devtomx@gmail.com>
2023-04-02 16:04:35 -0300

    ch8 exercise reverb2

---
+++ ch8/reverb2/reverb.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Println(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Println(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Println(c, "\t", strings.ToUpper(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
